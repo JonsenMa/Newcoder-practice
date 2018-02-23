bf5119c6730bbfa610fac0859e3d2041491e6a37
Marlocy <mjx690621749@163.com>
2018-02-23 22:29:22 +0800

    Add files via upload
    统计输入字符串中的中文字符个数并输出
    中文字符占两字节，每字节最高位为1，因此可判断两个字节是否都为负数，若都为负数说明为中文字符

---
+++ chinese.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int l,num=0;
+	char s[10];
+	scanf("%s",s);
+	l = strlen(s);
+	printf("%d\n", l);
+	for (int i = 0; i < l; ++i)
+	{
+		/* code */
+		if (s[i]<0)
+		{
+			num++;
+			printf("%d\t%d\n", i, num);
+		}
+	}
+	printf("Total Chinese Character(s):\t%d\n", (num/2));
+	return 0;
+}
